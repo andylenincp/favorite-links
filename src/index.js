01280c2fb867e6f4e025bea91581b4d5880fcc1d
Andy Castillo <andy.castillopalma@outlook.com>
2021-05-01 16:17:22 -0500

    configuraciones hbs

--- src/index.js
+++ src/index.js
@@ -1,20 +1,38 @@
 const express = require('express');
 const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const path = require('path');
 
 // Inicializaciones
 const app = express();
 
 // Settings
 app.set('title', 'Favorite links');
+app.set('views', path.join(__dirname, 'views'));
 app.set('port', process.env.PORT || 4000);
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+    helpers: require('./lib/handlebars')
+}));
+app.set('view engine', '.hbs');
 
 // Middlewares
 app.use(morgan('dev'));
+app.use(express.urlencoded({
+    extended: false
+}));
+app.use(express.json());
 
 // Global variables
+app.use((req, res, next) => {
+    next();
+});
 
 // Routes
-app.use(require('./routes/index'));
+app.use(require('./routes'));
 
 // Public
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Funcionando');
+    res.json('Funcionando');
 });
 
 module.exports = router;
