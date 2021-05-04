3a80f27d15c056c7275dcdedaaa39d2628ca2ad0
Andy Castillo <andy.castillopalma@outlook.com>
2021-05-04 11:37:00 -0500

    DB MySQL remota

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,8 @@
 module.exports = {
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'links'
+        host: 'remotemysql.com',
+        user: '6rsJYp3QWh',
+        password: 'X5kM54QlNs',
+        database: '6rsJYp3QWh'
     }
 }
